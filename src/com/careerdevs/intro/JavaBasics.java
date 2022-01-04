c4eb114bf2092f9f1cd1b85a42eed4b2b4ed12a5
darielhernandez <13dariel@gmail.com>
2022-01-03 20:37:45 -0500

    Java Basics Challenge 1/3

---
+++ src/com/careerdevs/intro/JavaBasics.java
@@ -0,0 +1,10 @@
+package com.careerdevs.intro;
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        String firstName= "Dariel";
+        String lastName= "Hernandez";
+        String fullName= firstName+lastName;
+        System.out.println(fullName);
+    }
+}
